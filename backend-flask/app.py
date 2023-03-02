1ef1222bc83dbc06fb38905bc5eaab435caf2ae6
hazemshaalan <hazem012345@outlook.com>
2023-03-02 10:52:25 +0000

    add rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,6 +28,13 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 import watchtower
 import logging
 from time import strftime
+#ROLLBAR
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
+
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
@@ -58,6 +65,29 @@ app = Flask(__name__)
 
 # X-RAY ----------
 #XRayMiddleware(app, xray_recorder)
+
+
+
+#RollBar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+         # send exceptions from `app` to rollbar, using flask's signal system.
+got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def hello():
+    return "Hello World!"
 
 
 # HoneyComb ---------

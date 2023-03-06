361f26eb4a76aeb9752661d62422a574d3d9cb6a
hazemshaalan <hazem012345@outlook.com>
2023-03-06 19:34:59 +0000

    implement backend check for the token

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,9 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+import sys
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 from services.home_activities import *
 from services.user_activities import *
 from services.create_activity import *
@@ -85,6 +88,13 @@ def init_rollbar():
          # send exceptions from `app` to rollbar, using flask's signal system.
 got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
+
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
+
 @app.route('/rollbar/test')
 def hello():
     return "Hello World!"
@@ -106,8 +116,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -154,7 +164,22 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
+
+     
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 def data_handle(handle):

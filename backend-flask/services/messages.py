a89a17e9c60d1e4e7f15e47575f9078fc2501e5c
hazemshaalan <hazem012345@outlook.com>
2023-03-31 22:23:41 +0000

    updated db.py for fixing errors

--- backend-flask/services/messages.py
+++ backend-flask/services/messages.py
@@ -1,25 +1,24 @@
 from datetime import datetime, timedelta, timezone
-class Messages:
-  def run(message_group_uuid,cognito_user_id):
+class MessageGroups:
+  def run(user_handle):
     model = {
       'errors': None,
       'data': None
     }
 
-    sql = db.template('users','uuid_from_cognito_user_id')
-    my_user_uuid = db.query_value(sql,{
-      'cognito_user_id': cognito_user_id
-    })
-
-    print(f"UUID: {my_user_uuid}")
-
-    ddb = Ddb.client()
-    data = Ddb.list_messages(ddb, message_group_uuid)
-    print("list_messages")
-    print(data)
-
-
-    
-    model['data'] = data
-
+    now = datetime.now(timezone.utc).astimezone()
+    results = [
+      {
+        'uuid': '24b95582-9e7b-4e0a-9ad1-639773ab7552',
+        'display_name': 'Andrew Brown',
+        'handle':  'andrewbrown',
+        'created_at': now.isoformat()
+      },
+      {
+        'uuid': '417c360e-c4e6-4fce-873b-d2d71469b4ac',
+        'display_name': 'Worf',
+        'handle':  'worf',
+        'created_at': now.isoformat()
+    }]
+    model['data'] = results
     return model

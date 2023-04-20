0b1e81b1ffc895b304e4679e110a25f0812fe17e
hazemshaalan <hazem012345@outlook.com>
2023-04-20 09:41:52 +0000

    almost get avatar uploading working

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -17,7 +17,7 @@ export default function ProfileForm(props) {
   const s3uploadkey = async (event)=> {
     try {
       console.log('s3upload')
-      const backend_url = "https://clbx5fa2yb.execute-api.ca-central-1.amazonaws.com/avatars/key_upload"
+      const backend_url = "https://hbf8753k0b.execute-api.us-east-1.amazonaws.com/avatars/key_upload"
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
@@ -127,6 +127,12 @@ export default function ProfileForm(props) {
             </div>
           </div>
           <div className="popup_content">
+
+          <div className="upload" onClick={s3uploadkey}>
+              Upload Avatar
+            </div>
+            
+          <input type="file" name="avatarupload" onChange={s3upload} />      
             <div className="field display_name">
               <label>Display Name</label>
               <input

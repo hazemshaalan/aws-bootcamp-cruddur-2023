53342ee14880339b716e999aebe8ac26e97d64fd
hazemshaalan <hazem012345@outlook.com>
2023-06-07 17:13:36 +0000

    fix seed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,19 +2,18 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('hazemshaalan','hazem01234@gmail.com' , 'hazemshaalan' ,'MOCK'),
-  ('hazemshaalan1','hazem012345@outlook.com' , 'hazemshaalan1' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+  ('hazemshaalan1','hazem012345@outlook.com' , 'hazemshaalan1' ,'MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'hazemshaalan' LIMIT 1),
-    'This was imported as seed data!',
+    'This was imported as seed data for original hazem!',
     current_timestamp + interval '10 day'
   ),
   (
     (SELECT uuid from public.users WHERE users.handle = 'hazemshaalan1' LIMIT 1),
-    'I am the other!',
+    'I am the other hazem!',
     current_timestamp + interval '10 day'
   );
